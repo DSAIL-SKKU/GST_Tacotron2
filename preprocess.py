ba94c8f52c963a0e380c642c05875bbcc5e1c337
yoonjeewoo <yoonjeewoo@gmail.com>
2020-01-16 16:32:17 +0900

    [JW] preprocess bible

--- preprocess.py
+++ preprocess.py
@@ -22,6 +22,14 @@ def preprocess_ljspeech(args):
     write_metadata(metadata, out_dir)
 
 
+def preprocess_bible(args):
+    in_dir = os.path.join(args.base_dir, 'bible')
+    out_dir = os.path.join(args.base_dir, args.output)
+    os.makedirs(out_dir, exist_ok=True)
+    metadata = ljspeech.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+    write_metadata(metadata, out_dir)
+
+
 def write_metadata(metadata, out_dir):
     with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
         for m in metadata:
@@ -37,13 +45,15 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
     parser.add_argument('--output', default='training')
-    parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech'])
+    parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'bible'])
     parser.add_argument('--num_workers', type=int, default=cpu_count())
     args = parser.parse_args()
     if args.dataset == 'blizzard':
         preprocess_blizzard(args)
     elif args.dataset == 'ljspeech':
         preprocess_ljspeech(args)
+    elif args.dataset == 'bible':
+        preprocess_bible(args)
 
 
 if __name__ == "__main__":
