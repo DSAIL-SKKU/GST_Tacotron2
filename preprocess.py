14129521ef48b3cbe111aced2cf00511a17d70e8
yoonjeewoo <yoonjeewoo@gmail.com>
2020-01-17 19:10:45 +0900

    [JW] preprocess bible

--- preprocess.py
+++ preprocess.py
@@ -43,7 +43,7 @@ def write_metadata(metadata, out_dir):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
+    parser.add_argument('--base_dir', default=os.path.expanduser('~/jeewoo/2020/Speech-Synthesis/tacotron/'))
     parser.add_argument('--output', default='training')
     parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'bible'])
     parser.add_argument('--num_workers', type=int, default=cpu_count())
