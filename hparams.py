1e37e3cf657ed236472c9cf93ae7aa1acc29e08a
yoonjeewoo <yoonjeewoo@gmail.com>
2020-01-18 14:20:11 +0900

    [JW] preprocess bible

--- hparams.py
+++ hparams.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 hparams = tf.contrib.training.HParams(
     # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
     # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
-    cleaners='english_cleaners',
+    cleaners='korean_cleaners',
     # Audio:
     num_mels=80,
     num_freq=1025,
