7560d4cbf303198d6e62e352a51422f7e893e45e
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-01-22 20:01:28 +0900

    Update datafeeder.py

--- datasets/datafeeder.py
+++ datasets/datafeeder.py
@@ -110,7 +110,8 @@ class DataFeeder(threading.Thread):
         input_data = np.asarray(text_to_sequence(text, self._cleaner_names), dtype=np.int32)
         linear_target = np.load(os.path.join(self._datadir, meta[0]))
         mel_target = np.load(os.path.join(self._datadir, meta[1]))
-        return (input_data, mel_target, linear_target, len(linear_target))
+        stop_token_target = np.asarray([0.] * len(mel_target))
+        return (input_data, mel_target, linear_target, stop_token_target, len(linear_target))
 
     def _maybe_get_arpabet(self, word):
         arpabet = self._cmudict.lookup(word)
