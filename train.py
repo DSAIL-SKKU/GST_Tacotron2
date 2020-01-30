4b9509f605c1cd5204a42390bd126405e84719c3
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-01-30 13:45:45 +0900

    Update train.py

--- train.py
+++ train.py
@@ -149,6 +149,7 @@ def main():
 	os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 	run_name = args.name or args.model
 	attention_name = hparams.attention_type
+	print(attention_name)
 	log_dir = os.path.join(args.base_dir, 'logs-%s-%s' % run_name, attention_name)
 	os.makedirs(log_dir, exist_ok=True)
 	infolog.init(os.path.join(log_dir, 'train.log'), run_name, args.slack_url)
