425acffb16fa870691a3b510340694dca9b045c7
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-03 01:25:06 +0900

    Update train.py

--- train.py
+++ train.py
@@ -148,12 +148,12 @@ def main():
 	os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 	os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 	run_name = args.name or args.model
+	hparams.parse(args.hparams)
 	attention_name = hparams.attention_type
 	print(attention_name)
 	log_dir = os.path.join(args.base_dir, 'logs-%s-%s' % (run_name, attention_name))
 	os.makedirs(log_dir, exist_ok=True)
 	infolog.init(os.path.join(log_dir, 'train.log'), run_name, args.slack_url)
-	hparams.parse(args.hparams)
 	train(log_dir, args)
 
 
