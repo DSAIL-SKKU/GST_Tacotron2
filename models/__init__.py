d3d79f515d95be06d350f2b1a3941762ab461dc2
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-08 18:27:27 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from .tacotron import Tacotron
 def create_model(name, hparams):
   if hparams.model == 'tacotron':
     return Tacotron(hparams)
-  elif hparams.model == 'tacotron2':
-    return Tacotron2(hparams)
+  # elif hparams.model == 'tacotron2':
+  #   return Tacotron2(hparams)
   else:
     raise Exception('Unknown model: ' + name)
